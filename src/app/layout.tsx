7601c6668f4968ec260c4e8ce8d75b878dd4a2f7
mohamed elamrani <mohammadamrani475@gmail.com>
2024-04-21 12:22:06 -0400

    build improvement

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,6 @@ function ToNav(){
     <nav className="flex items-center justify-between w-full p-4 text-xl font-semibold border-b">
       <div>Gallery</div>
       <div>Sign In</div>
-
     </nav>
 
   );
